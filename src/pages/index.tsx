1d5e6d1e9619916a63980304a9880b3d75248e1d
Akram Rasikh <akram.rasikh@gmail.com>
2024-03-26 12:14:43 +1100

    clean up

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,7 +5,7 @@ export default function Home(props) {
   const sentenceList = props?.satoriData;
   return (
     <div>
-      <ul style={{ listStyleType: 'none' }}>
+      <ul style={{ listStyleType: 'none', padding: '5px' }}>
         {sentenceList?.map((sentenceData, index) => {
           const sentence = sentenceData[0];
           const textWithKanji = sentenceData[1];
@@ -128,40 +128,3 @@ export async function getStaticProps() {
     };
   }
 }
-
-// [
-//   {
-//     userID: 'cb73d4d1-ba4a-4f20-9623-e622bd07f4d7',
-//     userSetKey: 'cb73d4d1-ba4a-4f20-9623-e622bd07f4d7',
-//     cardId: '5284505f-1a74-42de-9531-43c6678e9ab6',
-//     entryId: 'd192e5a5-f3ef-470f-9c4e-de6c476c55eb',
-//     senseId: '32afc371-757f-40fe-9f5e-eb8779c074a4',
-//     cardType: 'JE',
-//     whenCreated: '2024-01-25T09:59:17.606073+00:00',
-//     expression:
-//       '{"id":"qnfeASICYeaKZtuChrRt","code":null,"paragraphs":[{"id":"wlBsqqmvCtUqdBSsJDnL","type":0,"sentences":[{"id":"VSLGUzCPPJOdMweDaaro","runs":[{"id":"PBPtvNdvFMiVOTaMcXRS","parts":[{"id":"DuhXqeJNuXGqPbmNiiNr","kanjiKnowledge":0,"form":1,"parts":[{"id":"VkedCOQjIsbPHpeuDevC","text":"辺","reading":"あた"},{"id":"ONXpcjxZQAVWkRklaRjD","text":"り","reading":null},{"id":"iLijQdrnOGukzbfWHAkJ","text":"一面","reading":"いちめん"}]}]}],"audioPosition":0}]}]}',
-//     definition:
-//       '{"isContextDemonstrative":true,"senses":[{"isContextSense":true,"id":"32afc371-757f-40fe-9f5e-eb8779c074a4","partsOfSpeech":"exp","notes":"","glosses":"all around; as far as the eye can see"}],"id":"d192e5a5-f3ef-470f-9c4e-de6c476c55eb"}',
-//     contexts: [
-//       {
-//         userID: null,
-//         userSetKey: null,
-//         cardId: '5284505f-1a74-42de-9531-43c6678e9ab6',
-//         whenCreated: '2024-01-25T09:59:29.0286297+00:00',
-//         sentenceId: 'VrvmGlgYawEtioCHhyjJ', <---
-//         articleCode: 'oku-nikkou-episode-26-edition-n', <---
-//         audio: {
-//           isAccessibleForUser: true,
-//           isAvailableLocally: false,
-//         },
-//       },
-//     ],
-//     totalCorrect: 4,
-//     totalIncorrect: 0,
-//     consecutiveCorrect: 4,
-//     ef: 2.5,
-//     q: 5,
-//     whenUpdated: '2024-02-21T12:32:20.27918+00:00',
-//     nextDueDate: '2024-03-21T02:00:00+00:00',
-//   },
-// ];
