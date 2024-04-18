28880cc7e2000dde7d21956191094e849a4cbf9c
Akram Rasikh <akram.rasikh@gmail.com>
2024-04-18 17:31:10 +1000

    response functionality working for audio

--- src/components/ResponseSection.tsx
+++ src/components/ResponseSection.tsx
@@ -1,13 +1,40 @@
 const ResponseItem = ({ responseItem }) => {
+  let japaneseBareText = '';
+  const japaneseRegex = /\[JP\]/;
+
+  const handleGetAudio = async () => {
+    if (!japaneseBareText) return null;
+    try {
+      // figure reference to code
+      await fetch('/api/chatgpt-tts', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ tts: japaneseBareText }),
+      });
+    } catch (error) {
+      console.error('## Error fetching data:', error);
+    }
+  };
+
   return (
     <div style={{ borderTop: '1px solid grey' }}>
       {responseItem.split('\n').map((detail, index) => {
+        const isJapaneseText = japaneseRegex.test(detail);
+        if (isJapaneseText) {
+          japaneseBareText = detail.replace(japaneseRegex, '');
+        }
+
         if (!detail) {
           return null;
         }
         return (
           <li key={index}>
             <p>{detail}</p>
+            {isJapaneseText && (
+              <button onClick={handleGetAudio}>Get Audio</button>
+            )}
           </li>
         );
       })}
--- src/pages/api/chatgpt-tts.ts
+++ src/pages/api/chatgpt-tts.ts
@@ -4,25 +4,29 @@ const path = require('path');
 const OpenAI = require('openai');
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_OPENAI_API_KEY,
   dangerouslyAllowBrowser: true,
 });
 
-const tttt = `京子は、ベランダの柵によじ登り、硬直した体でワイシャツを掴んだ。風で飛ばされたかな、と思い、外を見回しても該当するものは見当たらなかった。その時、嫌なことがあると、すぐに泣き喚く子供が玄関へ走っていくのを目撃した。
-
-それ以来、ことあるごとに康平を責ようにな。`;
-
 const speechFile = path.resolve('./siu.mp3');
 
-async function chatGptTTSAPI() {
-  const mp3 = await openai.audio.speech.create({
-    model: 'tts-1',
-    voice: 'alloy',
-    input: tttt,
-  });
-  console.log(speechFile);
-  const buffer = Buffer.from(await mp3.arrayBuffer());
-  await fs.promises.writeFile(speechFile, buffer);
-}
+export default async function handler(req, res) {
+  try {
+    const { tts } = req.body;
 
-export default chatGptTTSAPI;
+    const mp3 = await openai.audio.speech.create({
+      model: 'tts-1',
+      voice: 'alloy',
+      input: tts,
+    });
+
+    const buffer = Buffer.from(await mp3.arrayBuffer());
+    await fs.promises.writeFile(speechFile, buffer);
+    // Send the data as the response
+    res.status(200).json();
+  } catch (error) {
+    // If an error occurs, send an error response
+    console.error('Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+}
