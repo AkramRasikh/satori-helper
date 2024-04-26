e26c0435b7cf675c6d930a8b6ebfc1dad141486f
Akram Rasikh <akram.rasikh@gmail.com>
2024-04-27 08:26:06 +1000

    removed redundant

--- src/components/AudioPlayer/index.tsx
+++ src/components/AudioPlayer/index.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useRef } from 'react';
 import useAudioPlayer from './useAudioPlayer';
 import AudioPlayerElement from './AudioPlayerElement';
 
