84803c9e121ff09fe9d28ef8a74e06376dba688f
Akram Rasikh <akram.rasikh@gmail.com>
2024-05-03 15:13:46 +1000

    removed ref

--- src/components/ResponseSection/ResponseSectionContentContainer.tsx
+++ src/components/ResponseSection/ResponseSectionContentContainer.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useState } from 'react';
 import ResponseCTAs from '../ResponseCTAs';
 import AudioPlayer from '../AudioPlayer';
 import getChatGptTTS from '@/pages/api/tts-audio';
